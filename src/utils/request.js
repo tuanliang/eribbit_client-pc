3e1c9e691c28d0890a0360880d1b1e919e5b85e2
shiyi <2416976257@qq.com>
2023-01-24 15:24:00 +0800

    相应拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,6 +4,7 @@
 // 4.导出一个函数，调用当前的axios实例发请求，返回值promise
 import axios from 'axios'
 import store from '@/store'
+import router from '@/router'
 
 // 导出基准地址，原因：其他地方不是通过axios发请求的地方，也会用到基准地址
 export const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
@@ -25,5 +26,24 @@ instance.interceptors.request.use(config => {
     config.headers.Authorization = `Bearer ${profile.token}`
   }
 }, err => {
+  return Promise.reject(err)
+})
+
+// res=>res.data 取出data数据，将来调用接口的时候直接拿到的就是后台的数据
+instance.interceptors.response.use(res => res.data, err => {
+  // 401状态码，进入该函数
+  if (err.response && err.response.status == 401) {
+    // 1.清空无效用户信息
+    // 2.跳转到登录页面
+    // 3.跳转需要传参（当前路由地址）给登录页码
+    store.commit('user/setUser', {})
+    // router.push('/login?redirectUrl='+'当前路由地址')
+    // 当前路由地址 如果在组件里头：`/user?a=10` $route.path === /user $route.fullPath === /user?a=10
+    // js模块里：router.currentRoute.fullPath 就是当前路由地址  
+    // router.currentRoute是ref响应式数据 ，所以获取数据为 router.currentRoute.value.fullPath
+    // encodeURIComponent 转换uri编码，防止解析地址出问题
+    const fullPath = encodeURIComponent(router.currentRoute.value.fullPath)
+    router.push('/login?redirectUrl=' + fullPath)
+  }
   return Promise.reject(err)
 })
