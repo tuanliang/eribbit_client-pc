10f3934fe3178891176134a67b294ed34e3224d7
shiyi <2416976257@qq.com>
2023-01-24 19:02:14 +0800

    顶部通栏布局

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 顶部通栏 -->
-  <nav>顶部通栏</nav>
+  <appNavBar></appNavBar>
   <!-- 头部组件 -->
   <header>头部组件</header>
   <!-- 内容容器 -->
@@ -12,8 +12,12 @@
   <footer>底部组件</footer>
 </template>
 <script>
+import appNavBar from '@/components/app-navbar.vue'
 export default {
-  name: 'LayOut'
+  name: 'LayOut',
+  components: {
+    appNavBar
+  }
 }
 </script>
 <style scoped lang="less">
