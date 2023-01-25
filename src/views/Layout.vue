a93e45e7110aeb8a61c821fb9b71943820f19305
shiyi <2416976257@qq.com>
2023-01-25 16:19:18 +0800

    吸顶头部组件

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -3,6 +3,8 @@
   <AppNavBar></AppNavBar>
   <!-- 头部组件 -->
   <AppHeader></AppHeader>
+  <!-- 吸顶头部 -->
+  <AppHeaderSticky></AppHeaderSticky>
   <!-- 内容容器 -->
   <div class="app-body">
     <!-- 二级路由 -->
@@ -16,12 +18,14 @@ import AppNavBar from '@/components/app-navbar.vue'
 import AppHeader from '@/components/app-header.vue'
 import AppFooter from '@/components/app-footer.vue'
 import { useStore } from 'vuex'
+import AppHeaderSticky from '@/components/app-header-sticky.vue'
 export default {
   name: 'LayOut',
   components: {
     AppNavBar,
     AppHeader,
-    AppFooter
+    AppFooter,
+    AppHeaderSticky
   },
   // 获取分类数据
   setup () {
