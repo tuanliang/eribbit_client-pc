34cded98d6b11c1dceaf85df2a6ef3b96cbf499a
shiyi <2416976257@qq.com>
2023-01-26 11:45:04 +0800

    首页主体-面板封装

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,17 +6,26 @@
       <HomeCategory></HomeCategory>
       <!-- 轮播图 -->
       <HomeBanner></HomeBanner>
+      <!-- 新鲜好物 -->
+      <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
+        <template #right>
+          <XtxMore path="/"></XtxMore>
+          <!-- 面板内容 -->
+        </template>
+      </HomePanel>
     </div>
   </div>
 </template>
 <script>
 import HomeCategory from './components/home-category.vue'
 import HomeBanner from './components/home-banner.vue'
+import HomePanel from './components/home-panel.vue'
 export default {
   name: 'HomePage',
   components: {
     HomeCategory,
-    HomeBanner
+    HomeBanner,
+    HomePanel
   }
 }
 </script>
