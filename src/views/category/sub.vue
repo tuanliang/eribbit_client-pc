fa9f8418909be31dbdf5b74524af5d9cf102802e
shiyi <2416976257@qq.com>
2023-01-27 19:05:03 +0800

    二级类目-展示面包屑

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="top-category">SubCategoty</div>
+  <div class="top-category">
+    <div class="container">
+      <!-- 面包屑 -->
+      <SubBread></SubBread>
+    </div>
+  </div>
 </template>
 
 <script>
+import SubBread from './components/sub-bread.vue'
 export default {
-  name: 'SubCategoty'
+  name: 'SubCategoty',
+  components: { SubBread }
 }
 </script>
 
