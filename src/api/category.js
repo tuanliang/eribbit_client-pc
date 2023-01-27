5cdae721677703464d3f83de1888ab2772911301
shiyi <2416976257@qq.com>
2023-01-27 19:58:08 +0800

    二级类目-筛选去展示

--- src/api/category.js
+++ src/api/category.js
@@ -9,6 +9,20 @@ export const findAllCategory = () => {
   return request('/home/category/head', 'get')
 }
 
+/**
+ * 获取顶级类目信息，（children属性就是各个子分类
+ * @param {String} id - 顶级类目iD
+ * @returns
+ */
 export const findTopCategory = (id) => {
   return request('/category', 'get', { id })
+}
+
+/**
+ * 获取二级类目的筛选条件数据
+ * @param {String} id -二级类目ID
+ * @returns
+ */
+export const findSubCategoryFilter = (id) => {
+  return request('/category/sub/filter', 'get', { id })
 }
