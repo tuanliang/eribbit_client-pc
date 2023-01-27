08f6f6285ffdf1dbef17c21318d8fd2a4bcbfb8e
shiyi <2416976257@qq.com>
2023-01-27 17:29:24 +0800

    顶级类目-基础布局搭建

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -3,23 +3,104 @@
     <div class="container">
       <!-- 面包屑 -->
       <XtxBread>
-        <XtxBreadItem :to="{ path: '/' }">首页</XtxBreadItem>
-        <XtxBreadItem to="/category/100501">电器</XtxBreadItem>
-        <XtxBreadItem>空调</XtxBreadItem>
+        <XtxBreadItem to="/">首页</XtxBreadItem>
+        <XtxBreadItem>{{ topCategory.name }}</XtxBreadItem>
       </XtxBread>
       <!-- 轮播图 -->
-      <!-- 全部分类 -->
+      <XtxCarousel :sliders="sliders" style="height:500px" />
+      <!-- 所有二级分类 -->
+      <div class="sub-list" v-if="topCategory && topCategory.children">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="sub in topCategory.children" :key="sub.id">
+            <a href="javascript:;">
+              <img :src="sub.picture">
+              <p>{{ sub.name }}</p>
+            </a>
+          </li>
+        </ul>
+      </div>
       <!-- 各个分类推荐商品 -->
     </div>
   </div>
 </template>
 
 <script>
+import { ref, computed } from 'vue'
+import { findBanner } from '@/api/home'
+import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
 export default {
-  name: 'TopCategoty'
+  name: 'TopCategoty',
+  setup () {
+    // 轮播图
+    const sliders = ref([])
+    findBanner().then(data => {
+      sliders.value = data.result
+    })
+
+    // 面包屑+所有子分类  ===>vuex
+    const store = useStore()
+    const route = useRoute()
+    const topCategory = computed(() => {
+      // 当前顶级分类 === 根据路由上的id去vuex中category模块的list中查找
+      let cate = {}
+
+      const item = store.state.category.list.find(item => {
+        return item.id === route.params.id
+      })
+      if (item) cate = item
+      return cate
+    })
+
+    return { sliders, topCategory }
+  }
 }
 </script>
 
 <style scoped lang="less">
+.top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
 
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+
+      li {
+        width: 168px;
+        height: 160px;
+
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+
+          img {
+            width: 100px;
+            height: 100px;
+          }
+
+          p {
+            line-height: 40px;
+          }
+
+          &:hover {
+            color: @xtxColor;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
