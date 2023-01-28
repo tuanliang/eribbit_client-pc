053f6fa8654a45b382facf32373333d3df779bb8
shiyi <2416976257@qq.com>
2023-01-28 20:00:48 +0800

    商品详情-渲染面包屑

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ const LayOut = () => import('@/views/Layout')
 const Home = () => import('@/views/home')
 const TopCategory = () => import('@/views/category/index')
 const SubCategory = () => import('@/views/category/sub')
+const Goods = () => import('@/views/goods/index')
 
 // 路由规则
 const routes = [
@@ -14,7 +15,8 @@ const routes = [
     children: [
       { path: '/', component: Home },
       { path: '/category/:id', component: TopCategory },
-      { path: '/category/sub/:id', component: SubCategory }
+      { path: '/category/sub/:id', component: SubCategory },
+      { path: '/product/:id', component: Goods }
     ]
   }
 ]
