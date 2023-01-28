3acad78a61112f6ec6f402cc7cddd06a9e2e7c77
shiyi <2416976257@qq.com>
2023-01-28 16:52:32 +0800

    二级类目-结果区-排序组件

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -5,8 +5,10 @@
       <SubBread></SubBread>
       <!-- 筛选区 -->
       <SubFilter></SubFilter>
-      <!-- 测试复选框 -->
-      <XtxCheckbox v-model="isAllChecked">全选</XtxCheckbox>{{ isAllChecked }}
+      <!-- 商品面板（排序+列表） -->
+      <div class="goods-list">
+        <SubSort></SubSort>
+      </div>
     </div>
   </div>
 </template>
@@ -14,10 +16,11 @@
 <script>
 import SubBread from './components/sub-bread.vue'
 import SubFilter from './components/sub-filter.vue'
+import SubSort from './components/sub-sort.vue'
 import { ref } from 'vue'
 export default {
   name: 'SubCategoty',
-  components: { SubBread, SubFilter },
+  components: { SubBread, SubFilter, SubSort },
   setup () {
     const isAllChecked = ref(true)
     return { isAllChecked }
@@ -26,5 +29,9 @@ export default {
 </script>
 
 <style scoped lang="less">
-
+.goods-list {
+  background: #fff;
+  padding: 0 25px;
+  margin-top: 25px;
+}
 </style>
