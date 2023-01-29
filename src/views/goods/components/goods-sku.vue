6acecb2b31d76454e9e357beedfaf2845815e465
shiyi <2416976257@qq.com>
2023-01-29 18:51:12 +0800

    规格组件-禁用效果（路径字典）

--- src/views/goods/components/goods-sku.vue
+++ src/views/goods/components/goods-sku.vue
@@ -13,6 +13,7 @@
   </div>
 </template>
 <script>
+import powerSet from '@/vender/power-set'
 export default {
   name: 'GoodsSku',
   props: {
@@ -21,7 +22,10 @@ export default {
       default: () => ({})
     }
   },
-  setup () {
+  setup (props) {
+    const pathMap = getPathMap(props.goods.skus)
+    console.log(pathMap)
+
     // 1.选中与取消选中，约定在每一个按钮都拥有自己的选中状态数据：selected
     const changeSku = (item, val) => {
       if (val.selected) {
@@ -35,6 +39,36 @@ export default {
     }
     return { changeSku }
   }
+}
+const spliter = '★'
+// 得到一个路径字典对象
+const getPathMap = (skus) => {
+  // 1.得到所有的sku集合 props.goods.skus
+  // 2.从所有的sku中筛选出有效的sku
+  // 3.根据有效的sku使用power-set算法得到子集
+  // 4.根据子集往路径字典对象中存储key-value
+  const pathMap = {}
+  skus.forEach(sku => {
+    if (sku.inventory > 0) {
+      // 计算当前有库存的sku的子集（幂集）
+      // 例如：[1,2,3]==>[[1],[2],[3],[1,2],[1,3],[2,3],[1,2,3]]
+      const valueArr = sku.specs.map(val => val.valueName)
+      // 可选值数组 幂集
+      const valueArrPowerSet = powerSet(valueArr)
+      // 遍历幂集 往pathMap插入数据
+      valueArrPowerSet.forEach(arr => {
+        // 根据arr得到字符串的key，约定key是：['蓝色'，'美国']===》'蓝色*美国'
+        const key = arr.join(spliter)
+        // 给pathMap设置数据
+        if (pathMap[key]) {
+          pathMap[key].push(sku.id)
+        } else {
+          pathMap[key] = [sku.id]
+        }
+      })
+    }
+  })
+  return pathMap
 }
 </script>
 <style scoped lang="less">
