42fb5c3e717a3d0f93d35810a2c7d2e2e11ba4ab
shiyi <2416976257@qq.com>
2023-01-30 16:41:35 +0800

    商品详情-同类推荐组件

--- src/api/product.js
+++ src/api/product.js
@@ -9,3 +9,13 @@ import request from "@/utils/request"
 export const findGoods = (id) => {
   return request('/goods', 'get', { id })
 }
+
+/**
+ * 获取推荐商品 | 猜你喜欢
+ * @param {String} id - 商品ID，传入id是商品推荐，不传是猜你喜欢
+ * @param {Interger} limit - 商品数量
+ * @returns
+ */
+export const findRelevantGoods = ({ id, limit = 16 }) => {
+  return request('/goods/relevant', 'get', { id, limit })
+}
