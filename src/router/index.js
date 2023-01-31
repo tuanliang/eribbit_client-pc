e70854ef6148b68dd8695e90a60f20bf670e6f5b
shiyi <2416976257@qq.com>
2023-01-31 17:11:09 +0800

    登录-基础布局

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ const Home = () => import('@/views/home')
 const TopCategory = () => import('@/views/category/index')
 const SubCategory = () => import('@/views/category/sub')
 const Goods = () => import('@/views/goods/index')
+const Login = () => import('@/views/login/index')
 
 // 路由规则
 const routes = [
@@ -18,6 +19,10 @@ const routes = [
       { path: '/category/sub/:id', component: SubCategory },
       { path: '/product/:id', component: Goods }
     ]
+  },
+  {
+    path: '/login',
+    component: Login
   }
 ]
 
