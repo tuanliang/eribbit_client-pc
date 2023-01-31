e7970385eb359c716bab843eba7e890aee2f4f84
shiyi <2416976257@qq.com>
2023-01-31 12:09:13 +0800

    分页组件-03渲染组件和切换分页

--- src/components/library/xtx-pagination.vue
+++ src/components/library/xtx-pagination.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="xtx-pagination">
-    {{ paper.btnArr }}
-    <a href="javascript:;" class="disabled">上一页</a>
-    <span>...</span>
-    <a href="javascript:;" class="active">3</a>
-    <a href="javascript:;">4</a>
-    <a href="javascript:;">5</a>
-    <a href="javascript:;">6</a>
-    <a href="javascript:;">7</a>
-    <span>...</span>
-    <a href="javascript:;">下一页</a>
+    <a @click="myCurrentPage--" v-if="myCurrentPage > 1" href="javascript:;">上一页</a>
+    <a v-else href="javascript:;" class="disabled">上一页</a>
+    <span v-if="pager.start > 1">...</span>
+    <a @click="myCurrentPage = i" href="javascript:;" v-for="i in pager.btnArr" :key="i"
+      :class="{ active: i === myCurrentPage }">{{ i }}</a>
+    <span v-if="pager.end < pager.pageCount">...</span>
+    <a @click="myCurrentPage++" v-if="myCurrentPage < pager.pageCount" href="javascript:;">下一页</a>
+    <a v-else href="javascript:;" class="disabled">下一页</a>
   </div>
 </template>
 <script>
@@ -23,12 +21,12 @@ export default {
     // 1.约定按钮的个数 5 个
     const count = 5
     // 2.当前显示的页码
-    const myCurrentPage = ref(1)
+    const myCurrentPage = ref(2)
     // 3.总页数=总条数/每一页条数 向上取整
     const myTotal = ref(100)
     const myPageSize = ref(10)
     // 其他数据 （总页数，起始按钮，结束按钮，按钮数组）依赖上面数据得到
-    const paper = computed(() => {
+    const pager = computed(() => {
       // 总页数
       const pageCount = Math.ceil(myTotal.value / myPageSize.value)
       // 按钮个数和当前页码 ==》起始按钮，结束按钮，按钮数组
@@ -58,7 +56,7 @@ export default {
         end
       }
     })
-    return { myCurrentPage, paper }
+    return { myCurrentPage, pager }
   }
 }
 </script>
