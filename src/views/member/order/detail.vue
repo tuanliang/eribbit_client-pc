25392c6e384463ce1dc6310638e4b141af8adb39
shiyi <2416976257@qq.com>
2023-02-08 17:00:38 +0800

    订单详情-steps组件

--- src/views/member/order/detail.vue
+++ src/views/member/order/detail.vue
@@ -1,13 +1,35 @@
 <template>
-  <div class="member-order">
-    MemberDetail
+  <div class="member-order-detail" v-if="order">
+    <!-- 头部 -->
+    <DetailAction :order="order"></DetailAction>
+    <!-- 进度 -->
+    <DetailStep :order="order"></DetailStep>
+    <!-- 物流 -->
+    <!-- 信息 -->
   </div>
 </template>
 <script>
+import { findOrderDetail } from '@/api/order';
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+import DetailAction from './components/detail-action.vue'
+import DetailStep from './components/detail-step.vue'
 export default {
-  name: 'MemberOrder'
+  name: 'MemberOrder',
+  components: { DetailAction, DetailStep },
+  setup () {
+    const route = useRoute()
+    const order = ref(null)
+    findOrderDetail(route.params.id).then(data => {
+      order.value = data.result
+    })
+    return { order }
+  }
 }
 </script>
 <style scoped lang="less">
-
+.member-order-detail {
+  background: #fff;
+  height: 100%;
+}
 </style>
